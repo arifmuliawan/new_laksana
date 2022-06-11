26bbede758b29a17db8034d4b2c629ea955a9fc6
arifmuliawan <arif.muliawan17@gmail.com>
2022-06-11 22:31:03 +0700

    Manembahkan file connection.php untuk koneksi ke DB

---
+++ administrator/connection.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+error_reporting(0);
+$my_server	= "localhost";
+$my_user 	= "root";
+$my_password= "";
+$my_db		= "laksana"; 	
+$con 		= new mysqli($my_server,$my_user,$my_password,$my_db);
+// Check connection
+if ($con -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $con -> connect_error;
+  exit();
+}
+$base_url	= 'http://localhost/new-laksana/';
+?>
