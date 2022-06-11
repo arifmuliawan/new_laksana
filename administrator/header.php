954a9e1271690ba9767ca1076d910ae4e4bbfde2
arifmuliawan <arif.muliawan17@gmail.com>
2022-06-11 22:34:54 +0700

    Manambahkan file header.php untuk menampilkan header administrator page

---
+++ administrator/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <title>Laksana - Administrator</title>
+    <!-- Favicon
+    <link rel="icon" href="assets/images/laksana/icomoon.woff2" type="image/x-icon">-->
+    <!-- Plugins Core Css -->
+    <link href="assets/css/app.min.css" rel="stylesheet">
+    <!-- Custom Css -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- You can choose a theme from css/styles instead of get all themes -->
+    <link href="assets/css/styles/all-themes.css" rel="stylesheet" />
+    <script type="text/javascript" src="assets/ckeditor/ckeditor.js"></script>
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+
+</head>
+
+<body class="light">
+    <!-- Overlay For Sidebars -->
+   <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Top Bar -->
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header" style="height: 60px;">
+                <a class="navbar-brand" href="index.php">
+                   <font color="#fff" size="5">ADMINISTRATOR</font>
+                </a>
+            </div>
+        </div>
+        <!--
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="pull-left">
+                <li>
+                    <a href="#" onClick="return false;" class="sidemenu-collapse" style="margin-left: 232px;margin-top: -63px;">
+                        <i class="material-icons">reorder</i>
+                    </a>
+                </li>
+            </ul>
+        </div>  
+-->  
+    </nav>
