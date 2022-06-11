7e71afcff084dd0647697086e74f2b8540399449
arifmuliawan <arif.muliawan17@gmail.com>
2022-06-11 22:37:56 +0700

    Manambahkan file footer.php untuk menampilkan footer pada administrator page

---
+++ administrator/footer.php
@@ -0,0 +1,14 @@
+<script src="assets/js/app.min.js"></script>
+    <script src="assets/js/admin.js"></script>
+    <!-- Custom Js Tables -->
+	<script src="assets/js/table.min.js"></script>
+	<script src="assets/js/pages/tables/jquery-datatable.js"></script>
+	<!-- Custom Js Form -->
+	<script src="assets/js/pages/forms/form-validation.js"></script>
+	<script src="assets/js/pages/forms/basic-form-elements.js"></script>
+	<script src="assets/js/bundles/ckeditor/ckeditor.js"></script>
+	<script src="assets/js/pages/forms/editors.js"></script>
+}
+</body>
+
+</html>
