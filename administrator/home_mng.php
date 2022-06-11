ad735809fc48aad87e68932cc9f138de4b256f35
arifmuliawan <arif.muliawan17@gmail.com>
2022-06-12 00:10:48 +0700

    Manambahkan file home_mng.php untuk mengelola content halaman homepage di website laksana

---
+++ administrator/home_mng.php
@@ -0,0 +1,45 @@
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <ul class="breadcrumb breadcrumb-style ">
+                        <li class="breadcrumb-item">
+                            <h4 class="page-title"><font color="rgb(41, 41, 43)">Home Management</font></h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="body">
+                            <!-- Nav tabs -->
+                            <ul class="nav nav-tabs tab-nav-right" role="tablist">
+                                <li>
+                                    <a href="index.php?p=home&t=highlight" <?php if($tab=='' || $tab=='highlight'){ echo 'class="active show"';} ?>>Highlight</a>
+                                </li>
+                                <li>
+                                    <a href="index.php?p=home&t=banner" <?php if($tab=='banner'){ echo 'class="active show"';} ?>>Banner</a>
+                                </li>
+                                <li>
+                                    <a href="index.php?p=home&t=tagging" <?php if($tab=='tagging'){ echo 'class="active show"';} ?>>Tagging</a>
+                                </li>
+                                <li>
+                                    <a href="index.php?p=home&t=individual" <?php if($tab=='individual'){ echo 'class="active show"';} ?>>Individual Series</a>
+                                </li>
+                            </ul>
+                            <?php
+                            if($tab=='')
+                            {
+                                $tab    = 'highlight';
+                            }
+                            include('home_'.$tab.'.php');
+                            ?>      
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>
+</section>
